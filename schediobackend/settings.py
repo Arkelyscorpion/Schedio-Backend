0c9615545aa1d07b9bf16a212a8ba934e7dbf990
rishiyanth <apprishiategood002@gmail.com>
2023-01-15 19:01:07 +0530

    remodfied gitignore

--- schediobackend/settings.py
+++ schediobackend/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
     'ENGINE' : 'django.db.backends.postgresql_psycopg2',
     'NAME' : 'Schedio',
     'USER' : 'postgres',
-    'PASSWORD' : 'admin',
+    'PASSWORD' : 'rishiyanth',
     'HOST' : 'localhost',
     'PORT' : '5432',
     }
