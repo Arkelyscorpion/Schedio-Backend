2d1c1a4346e886816e6a3a562555e67610fe4159
Krishna Sameer <krishnasameer51@gmail.com>
2023-04-22 11:27:04 +0530

    Update settings.py

--- schediobackend/settings.py
+++ schediobackend/settings.py
@@ -167,6 +167,4 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'krishnasameer@student.tce.edu'
-EMAIL_HOST_PASSWORD = 'Ganges&Godavari'
 
